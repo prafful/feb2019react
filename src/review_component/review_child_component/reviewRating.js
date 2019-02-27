d30b4fbdb1e9106d3c3d102570052a8f5feae737
unknown <prraful@gmail.com>
2019-02-27 17:05:11 +0530

    review rating done with state implementation

--- src/review_component/review_child_component/reviewRating.js
+++ src/review_component/review_child_component/reviewRating.js
@@ -2,12 +2,31 @@ import React from 'react';
 
 
 class ReviewRating extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            like: parseInt(this.props.rrlike),
+            dislike: parseInt(this.props.rrdislike)
+        }
+
+        this.changeLike = this.changeLike.bind(this)
+        this.changeDisLike = this.changeDisLike.bind(this)
+    }
+
+    changeLike = function(){
+        this.setState({like: this.state.like + 1})
+    }
+
+    changeDisLike = function(){
+        this.setState({dislike: this.state.dislike - 1})
+    }
     
     render() { 
         return ( 
             <span>
-                <button> + </button> {this.props.rrlike}
-                <button> - </button> {this.props.rrdislike}
+                <button onClick={this.changeLike}> + </button> {this.state.like}
+                <button onClick={this.changeDisLike}> - </button> {this.state.dislike}
             </span>
          );
     }
